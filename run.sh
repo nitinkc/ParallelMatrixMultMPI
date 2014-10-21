3fd31e5d41ef045ed836fc1ab7865e78adf370a4
Nitin Chaurasia <nitin@bgsu.edu>
2014-10-21 00:09:38 -0400

    Overall speed up curves added

--- run.sh
+++ run.sh
@@ -1,11 +1,35 @@
 mpicc matrixMultMPI.c -o matrixMultMPI.o 
-echo "Speed up curve"
+echo "Speed up curve 5000,2000,5000"
 mpirun -hostfile machine -np 4 ./matrixMultMPI.o 5000 2000 5000
 mpirun -hostfile machine -np 8 ./matrixMultMPI.o 5000 2000 5000
 mpirun -hostfile machine -np 12 ./matrixMultMPI.o 5000 2000 5000
 mpirun -hostfile machine -np 16 ./matrixMultMPI.o 5000 2000 5000
 mpirun -hostfile machine -np 20 ./matrixMultMPI.o 5000 2000 5000
 mpirun -hostfile machine -np 44 ./matrixMultMPI.o 5000 2000 5000
+
+echo "Speed up curve 100 60 100"
+mpirun -hostfile machine -np 4 ./matrixMultMPI.o 100 60 100
+mpirun -hostfile machine -np 8 ./matrixMultMPI.o 100 60 100
+mpirun -hostfile machine -np 12 ./matrixMultMPI.o 100 60 100
+mpirun -hostfile machine -np 16 ./matrixMultMPI.o 100 60 100
+mpirun -hostfile machine -np 20 ./matrixMultMPI.o 100 60 100
+mpirun -hostfile machine -np 44 ./matrixMultMPI.o 100 60 100
+
+echo "Speed up curve 500 200 500"
+mpirun -hostfile machine -np 4 ./matrixMultMPI.o 500 200 500
+mpirun -hostfile machine -np 8 ./matrixMultMPI.o 500 200 500
+mpirun -hostfile machine -np 12 ./matrixMultMPI.o 500 200 500
+mpirun -hostfile machine -np 16 ./matrixMultMPI.o 500 200 500
+mpirun -hostfile machine -np 20 ./matrixMultMPI.o 500 200 500
+mpirun -hostfile machine -np 44 ./matrixMultMPI.o 500 200 500
+
+echo "Speed up curve 1000 500 1000"
+mpirun -hostfile machine -np 4 ./matrixMultMPI.o 1000 500 1000
+mpirun -hostfile machine -np 8 ./matrixMultMPI.o 1000 500 1000
+mpirun -hostfile machine -np 12 ./matrixMultMPI.o 1000 500 1000
+mpirun -hostfile machine -np 16 ./matrixMultMPI.o 1000 500 1000
+mpirun -hostfile machine -np 20 ./matrixMultMPI.o 1000 500 1000
+mpirun -hostfile machine -np 44 ./matrixMultMPI.o 1000 500 1000
 
 echo
 echo "Granularity effect with 24"
